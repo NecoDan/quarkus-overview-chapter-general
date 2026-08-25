9c4248b45ce7c6d91b37a1d074da95d4c352c43f
Daniel Santos <neco.daniel@gmail.com>
2026-08-24 21:09:43 -0300

    test: cover uncovered use case flows

---
+++ src/test/java/br/com/daniel/java/quarkus/general/core/usecase/itau_challenge/TransactionItauRemoveUseCaseImplTest.java
@@ -0,0 +1,34 @@
+package br.com.daniel.java.quarkus.general.core.usecase.itau_challenge;
+
+import br.com.daniel.java.quarkus.general.core.port.TransactionItauMemoryPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionItauRemoveUseCaseImplTest {
+
+    @Mock
+    TransactionItauMemoryPort transactionItauMemoryPort;
+
+    @InjectMocks
+    TransactionItauRemoveUseCaseImpl useCase;
+
+    @Test
+    void deletesTransactionById() {
+        useCase.deleteById("transaction-id");
+
+        verify(transactionItauMemoryPort).deleteById("transaction-id");
+    }
+
+    @Test
+    void deletesAllTransactions() {
+        useCase.deleteAll();
+
+        verify(transactionItauMemoryPort).deleteAll();
+    }
+}
